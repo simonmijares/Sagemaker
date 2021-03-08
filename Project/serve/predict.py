0aad43a2eada8e4851a476d70f5434574acd7efd
EC2 Default User <ec2-user@ip-172-16-8-206.ec2.internal>
2021-03-08 06:30:37 +0000

    Final commit

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -54,7 +54,7 @@ def input_fn(serialized_input_data, content_type):
     raise Exception('Requested unsupported ContentType in content_type: ' + content_type)
 
 def output_fn(prediction_output, accept):
-    print('Serializing the generated output.')
+    print('Serializing the generated output:')
     return str(prediction_output)
 
 def predict_fn(input_data, model):
@@ -69,9 +69,12 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
+    
     data_X = None
     data_len = None
+    
+    # SOLUTION:
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -88,5 +91,9 @@ def predict_fn(input_data, model):
     #       be a numpy array which contains a single integer which is either 1 or 0
 
     result = None
-
+    
+    # Solution:
+    
+    result = round(model(data).item())
+    
     return result
